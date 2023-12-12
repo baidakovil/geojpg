62f2558bd4115ec58cf288c5e79ae0e00aed7673
baidakovil <baidakovil@gmail.com>
2023-12-12 19:58:15 +0300

    upd: License notice

--- geojpg.py
+++ geojpg.py
@@ -1,4 +1,17 @@
-"""The program embed gps coordinates from gpx into jpg files on time eq-ty"""
+# geojpg — Utility for inserting GPX-routes geodata to JPG files.
+# Copyright (C) 2023 Ilia Baidakov <baidakovil@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <https://www.gnu.org/licenses/>.
+"""The program consist of single file geojpg.py. All the things are inside."""
 
 import os
 import re
